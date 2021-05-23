d0d948eb1ec947c3db921d6c349afb8246de58fb
Prabhat <singhprabhatp.s.224@gamil.com>
2021-05-23 16:11:47 +0530

    'changes'

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -5,19 +5,11 @@ import Main from '../Main';
 
 
 class App extends React.Component {
-  render(){
+  render() {
     return (
       <div className="App">
         <header className="App-header">
           <h2 className="App-title">TV-series-app</h2>
-          {/* <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a> */}
         </header>
         <Main />
       </div>
